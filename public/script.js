d55aa5ec04041cb8dfbce9d7682109fd536bc84f
JureArsovic <ja6533@student.uni-lj.si>
2023-11-07 20:31:13 +0100

    speech working

--- public/script.js
+++ public/script.js
@@ -1,9 +1,7 @@
-// Get the send button and input field elements from the DOM
 const sendButton = document.getElementById('sendButton');
 const inputField = document.getElementById('textInput');
 const responseContainer = document.getElementById('response');
 
-// Add a click event listener to the send button
 sendButton.addEventListener('click', function() {
     const userInput = inputField.value;
     //console.log("User input: ", userInput);
@@ -35,3 +33,33 @@ function displayResponse(text) {
 
   responseContainer.innerText = responseText;
 }
+
+if ('webkitSpeechRecognition' in window) {
+  var recognition = new webkitSpeechRecognition();
+
+  recognition.continuous = false; 
+  recognition.interimResults = true;
+  recognition.onresult = function(event) {
+      var transcript = event.results[0][0].transcript;
+      document.getElementById('textInput').value = transcript;
+  };
+
+  recognition.onstart = function() {
+      console.log("Voice recognition started. Speak into the microphone.");
+  };
+
+  recognition.onerror = function(event) {
+      console.log("Error occurred in recognition: " + event.error);
+  };
+
+  function startListening() {
+    //console.log("startListening");
+    recognition.start();
+  }
+  document.getElementById('speakButton').addEventListener('click', startListening);
+
+} else {
+  console.error("Speech recognition not supported in this browser.");
+}
+
+
